298b951dda777c26d7b760723ec3f17f192d88ab
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2024-06-27 17:31:29 -0400

    made cloud function for retreiving data

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,24 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.fetchUsers = functions.https.onRequest((req, res) => {
+  admin.firestore().collection("users").get()
+      .then((snapshot) => {
+        const users = [];
+        snapshot.forEach((doc) => {
+          const userData = doc.data();
+          userData.id = doc.id; // Add the document ID as the user ID
+          users.push(userData);
+        });
+        res.status(200).json(users);
+      })
+      .catch((error) => {
+        res.status(500).json({error: error.message});
+      });
 });
