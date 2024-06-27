921e35e66a5ecd0ae104d479d8dd86dfbf78cc1f
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2024-06-27 14:13:05 -0400

    Initial Commit

---
+++ BIPDemo/BIPDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIPDemoApp.swift
+//  BIPDemo
+//
+//  Created by Micheal Bingham on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BIPDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
